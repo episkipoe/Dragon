e365ec550eb253bb48f7ed9ec864df879b90d562
Dave Bennett <episkipoe@gmail.com>
2011-05-25 12:06:06 -0500

    add persistence:  save/load Player on startup/shutdown

---
+++ src/com/episkipoe/dragon/agents/AgentMate.java
@@ -0,0 +1,13 @@
+package com.episkipoe.dragon.agents;
+
+import java.io.Serializable;
+
+public class AgentMate implements Serializable {
+	private static final long serialVersionUID = 2396013698390026610L;
+	
+	Agent mother=null;
+	Agent father=null;
+	public Agent mate (Agent other) {
+		return null;
+	}
+}
